14851f2a00faa62c1e0b6c1382319604f234178a
AndresFelipeDzT <andresdiaztorres2@gmail.com>
2026-03-02 23:15:26 -0500

    FIX
    corrección de nombres

--- app/src/main/java/com/example/proyecto/screens/Team.kt
+++ app/src/main/java/com/example/proyecto/screens/Team.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -71,19 +72,33 @@ fun TeamScreen(navController: NavController) {
                     .height(140.dp)
                     .clickable {
                     navController.navigate(AppScreens.AccountScreen.name)
-                }
-
-                    ,
+                },
                 shape = RoundedCornerShape(24.dp),
                 colors = CardDefaults.cardColors(containerColor = Color.White),
                 elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.bulbasaur),
-                    contentDescription = "Bulbasaur",
-                    modifier = Modifier.fillMaxSize().padding(10.dp),
-                    contentScale = ContentScale.Fit
-                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.bulbasaur),
+                        contentDescription = "Bulbasaur",
+                        modifier = Modifier
+                            .height(80.dp)
+                            .padding(6.dp),
+                        contentScale = ContentScale.Fit
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Text(
+                        text = "Bulbasaur",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+
 
             }
 
@@ -100,13 +115,30 @@ fun TeamScreen(navController: NavController) {
                 colors = CardDefaults.cardColors(containerColor = Color.White),
                 elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.charmander),
-                    contentDescription = "Charmander",
-                    modifier = Modifier.fillMaxSize().padding(10.dp),
-                    contentScale = ContentScale.Fit
-                )
-            }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.charmander),
+                            contentDescription = "Charmander",
+                            modifier = Modifier
+                                .height(80.dp)
+                                .padding(6.dp),
+                            contentScale = ContentScale.Fit
+                        )
+                        Spacer(modifier = Modifier.height(6.dp))
+
+                        Text(
+                            text = "Charmander",
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+
+
+                }
 
 
             ElevatedCard(
@@ -121,12 +153,29 @@ fun TeamScreen(navController: NavController) {
                 colors = CardDefaults.cardColors(containerColor = Color.White),
                 elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.squirtle),
-                    contentDescription = "Squirtle",
-                    modifier = Modifier.fillMaxSize().padding(10.dp),
-                    contentScale = ContentScale.Fit
-                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.squirtle),
+                        contentDescription = "Squirtle",
+                        modifier = Modifier
+                            .height(80.dp)
+                            .padding(6.dp),
+                        contentScale = ContentScale.Fit
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Text(
+                        text = "Squirtle",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+
+
             }
         }
     }
